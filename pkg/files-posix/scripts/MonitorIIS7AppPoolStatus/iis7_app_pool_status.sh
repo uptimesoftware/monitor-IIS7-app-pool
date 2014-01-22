df298b8aa3caac79479885986621142f19f9246f
Balaji Natarajan <balajin.nsfz@gmail.com>
2014-01-22 14:33:24 +0530

    Set MIBDIRS env-var and fixed absolute php path in files-posix directory script

--- pkg/files-posix/scripts/MonitorIIS7AppPoolStatus/iis7_app_pool_status.sh
+++ pkg/files-posix/scripts/MonitorIIS7AppPoolStatus/iis7_app_pool_status.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/sh
-..\..\apache\php\php rcs.php -h=$UPTIME_HOSTNAME -p=$UPTIME_PORT -s=$UPTIME_PASSWORD -c="iis7_app_pool_status" -a="$UPTIME_APP_POOL"
+
+inst=`grep ^inst /etc/init.d/uptime_httpd | cut -d= -f2`
+MIBDIRS=$inst/mibs
+export MIBDIRS
+
+/usr/local/uptime/apache/bin/php rcs.php -h=$UPTIME_HOSTNAME -p=$UPTIME_PORT -s=$UPTIME_PASSWORD -c="iis7_app_pool_status" -a="$UPTIME_APP_POOL"
